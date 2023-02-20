0ec94f6222774694fb4b0dd1478d8d56ead107db
Bi Bamba <maxime-tizie@hotmail.fr>
2023-02-20 17:58:11 -0600

    add a mode line

--- use-of-os-module/create_dir.py
+++ use-of-os-module/create_dir.py
@@ -8,9 +8,12 @@ if len(args) != 3:
     raise Exception("You must enter a source and target directory")
 source, target = args[1:] 
 
+# mode 
+mode = 0o666
+
 # Path
 paths = os.path.join(source, target)
 
 # Create the directory
-os.mkdir(paths)
+os.mkdir(paths, mode)
 print("Directory '% s' created" % target)
