f05d29a372f183024269cb84a96bc0d0632a9238
Bi Bamba <maxime-tizie@hotmail.fr>
2023-02-23 14:05:00 -0600

    reorganise the code

--- use-of-os-module/create_dir.py
+++ use-of-os-module/create_dir.py
@@ -2,25 +2,31 @@
 import os
 import sys
 
-# command line argument
-args = sys.argv
-if len(args) != 3:
-    raise Exception("You must enter a source and target directory")
-source, target = args[1:] 
-
+def list_dir(given_dir):
 # list all the files and direcotry
-source_dir_list = os.listdir(source)
-print("Here are the directories in '% s'" % source)
-print(source_dir_list)
+    source_dir_list = os.listdir(given_dir)
+    print("Here are the contents of '% s'" % given_dir)
+    print(source_dir_list)
 
-# mode 
-mode = 0o666
+def create_dir(target_dir,new_dir):
+    # mode 
+    mode = 0o666
 
-# Path
-paths = os.path.join(source, target)
+    # Path
+    paths = os.path.join(target_dir, new_dir)
 
-# Create the directory
-os.mkdir(paths, mode)
-print("Directory '% s' created" % target)
+    # Create the directory
+    os.mkdir(paths, mode)
+    print("Directory '% s' is created" % new_dir)
 
+def main(source,target):
+    list_dir(source)
+    create_dir(source,target)
 
+if __name__ == "__main__":
+    # command line argument
+    args = sys.argv
+    if len(args) != 3:
+        raise Exception("You must enter a source and target directory")
+    source, target = args[1:] 
+    main(source,target)
