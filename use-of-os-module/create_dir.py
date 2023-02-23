88ab808b7c13db4138c3b34ddfca2980c751edbb
Bi Bamba <maxime-tizie@hotmail.fr>
2023-02-23 13:05:33 -0600

    add a permission to created directory

--- use-of-os-module/create_dir.py
+++ use-of-os-module/create_dir.py
@@ -8,6 +8,11 @@ if len(args) != 3:
     raise Exception("You must enter a source and target directory")
 source, target = args[1:] 
 
+# list all the files and direcotry
+source_dir_list = os.listdir(source)
+print("Here are the directories in '% s'" % source)
+print(source_dir_list)
+
 # mode 
 mode = 0o666
 
@@ -17,3 +22,5 @@ paths = os.path.join(source, target)
 # Create the directory
 os.mkdir(paths, mode)
 print("Directory '% s' created" % target)
+
+
